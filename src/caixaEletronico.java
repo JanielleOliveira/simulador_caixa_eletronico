4de85c1a9ff11d4e9c9ead6f5557bb419f8a4455
Janielle Oliveira <janielleoliver.brito@gmail.com>
2023-10-31 22:42:59 -0300

    Criação das Variáveis

---
+++ src/caixaEletronico.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class caixaEletronico {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Inicializando arrays para armazenar dados da conta
+        String[] numerosContas = new String[10];
+        String[] nomesTitulares = new String[10];
+        double[] saldos = new double[10];
+    }
+}
